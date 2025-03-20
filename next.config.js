37c9d4eda45ae87aad31d490df7c20ae0b79a021
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2025-03-19 23:20:20 -0500

    xd xd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
   images: {
     unoptimized: true,
     domains: [
